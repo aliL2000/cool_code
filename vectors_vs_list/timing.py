1ac72a1a0643028cfd0bf8441bbc314a4d744432
thenewguy321 <aliladha2000@gmail.com>
2023-09-16 00:16:23 -0400

    Adding different timings and testing

--- vectors_vs_list/timing.py
+++ vectors_vs_list/timing.py
@@ -1,16 +1,24 @@
 import time
+import numpy as np
+
+#Size of lengths to initialize and declare for both the list and vector
+size = 10_000_0000
 
 def create_and_update_list():
-    return 0
+    listfiller = []
+    for i in range(size):
+        listfiller.append(i)
 
 def create_and_update_vector():
-    return 0
+    arr = np.empty(size)
+    for i in range(size):
+        arr[i] = i
 
 #Start timing
 startlist = time.perf_counter()
 
 #perform any calculation
-
+create_and_update_list()
 #Re-run timing
 endlist = time.perf_counter()
 
@@ -18,9 +26,8 @@ endlist = time.perf_counter()
 startvector = time.perf_counter()
 
 #perform any calculation
-
+create_and_update_vector()
 #Re-run timing
 endvector = time.perf_counter()
 print(f"Performed calculation in {endlist - startlist:0.4f} seconds")
 print(f"Performed calculation in {endvector - startvector:0.4f} seconds")
-
