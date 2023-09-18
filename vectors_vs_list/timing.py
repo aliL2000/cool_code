a6a158dcf155bf3c1a5ae63e679fb4998a228808
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-09-18 17:57:27 -0400

    Adding only initialization

--- vectors_vs_list/timing.py
+++ vectors_vs_list/timing.py
@@ -4,21 +4,21 @@ import numpy as np
 #Size of lengths to initialize and declare for both the list and vector
 size = 10_000_0000
 
-def create_and_update_list():
+def create_list():
     listfiller = []
-    for i in range(size):
-        listfiller.append(i)
+    # for i in range(size):
+    #     listfiller.append(i)
 
-def create_and_update_vector():
+def create_vector():
     arr = np.empty(size)
-    for i in range(size):
-        arr[i] = i
+    # for i in range(size):
+    #     arr[i] = i
 
 #Start timing
 startlist = time.perf_counter()
 
 #perform any calculation
-create_and_update_list()
+create_and_update_vector()
 #Re-run timing
 endlist = time.perf_counter()
 
@@ -26,8 +26,8 @@ endlist = time.perf_counter()
 startvector = time.perf_counter()
 
 #perform any calculation
-create_and_update_vector()
+create_and_update_list()
 #Re-run timing
 endvector = time.perf_counter()
-print(f"Performed calculation in {endlist - startlist:0.4f} seconds")
-print(f"Performed calculation in {endvector - startvector:0.4f} seconds")
+print(f"Performed list calculation in {endlist - startlist:0.10f} seconds")
+print(f"Performed vector calculation in {endvector - startvector:0.10f} seconds")
